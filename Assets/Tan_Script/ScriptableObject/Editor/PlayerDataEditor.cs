dddb06657c66ae30a04d9e53387cc900c689c160
Tangoes98 <cyriltan985@gmail.com>
2023-09-17 00:07:16 -0400

    Add ProgressBar to playerData

--- Assets/Tan_Script/ScriptableObject/Editor/PlayerDataEditor.cs
+++ Assets/Tan_Script/ScriptableObject/Editor/PlayerDataEditor.cs
@@ -6,6 +6,35 @@ using UnityEditor;
 [CustomEditor(typeof(PlayerData))]
 public class PlayerDataEditor : Editor
 {
+    public override void OnInspectorGUI()
+    {
+        PlayerData data = (PlayerData)target;
+
+        EditorGUILayout.LabelField(data.Name.ToUpper(), EditorStyles.boldLabel);
+
+        EditorGUILayout.Space(10);
+        float playerAverageStrength =
+            data.Strength
+            + data.Dexterity
+            + data.Constitution
+            + data.Intelligence
+            + data.Wisdom
+            + data.Charisma;
+
+        ProgressBar(playerAverageStrength / 120, "AVERAGE STRENGTH");
+        // Add before
+        base.OnInspectorGUI();
+        // Add after
+
+        EditorGUILayout.LabelField("LABELFIELD HERE");
+
+    }
+    void ProgressBar(float value, string label)
+    {
+        Rect rect = GUILayoutUtility.GetRect(18, 18, "TextField");
+        EditorGUI.ProgressBar(rect, value, label);
+    }
+
 
 
 }
--- Assets/Tan_Script/ScriptableObject/PlayerData.cs
+++ Assets/Tan_Script/ScriptableObject/PlayerData.cs
@@ -8,6 +8,32 @@ using UnityEngine.Rendering;
 public class PlayerData : ScriptableObject
 {
     [SerializeField] string _name;
+
+    [Space(10)]
     [SerializeField] int _level;
     [SerializeField] float _exp;
+
+    [Space(10)]
+    [SerializeField][Range(0, 20)] int _strength;
+    [SerializeField][Range(0, 20)] int _dexterity;
+    [SerializeField][Range(0, 20)] int _constitution;
+    [SerializeField][Range(0, 20)] int _intelligence;
+    [SerializeField][Range(0, 20)] int _wisdom;
+    [SerializeField][Range(0, 20)] int _charisma;
+
+    [Space(10)]
+    [SerializeField] int _bonus;
+    [SerializeField] int _speed;
+
+
+
+    public string Name { get { return _name; } }
+    public int Level { get { return _level; } }
+    public float Exp { get { return _exp; } }
+    public int Strength { get { return _strength; } }
+    public int Dexterity { get { return _dexterity; } }
+    public int Constitution { get { return _constitution; } }
+    public int Intelligence { get { return _intelligence; } }
+    public int Wisdom { get { return _wisdom; } }
+    public int Charisma { get { return _charisma; } }
 }
