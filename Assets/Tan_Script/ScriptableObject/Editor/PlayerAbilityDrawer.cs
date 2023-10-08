35b1e3a3e82123fe3d9fcc8955c5c1e8f23a1e4f
Tangoes98 <cyriltan985@gmail.com>
2023-10-08 03:30:54 -0400

    Draw Property on GUI

--- Assets/Tan_Script/ScriptableObject/Editor/PlayerAbilityDrawer.cs
+++ Assets/Tan_Script/ScriptableObject/Editor/PlayerAbilityDrawer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UIElements;
 
 
 [CustomPropertyDrawer(typeof(PlayerAbility))]
@@ -17,13 +18,23 @@ public class PlayerAbilityDrawer : PropertyDrawer
 
         EditorGUI.BeginProperty(position, label, property);
 
+        _playerAttributes = property.FindPropertyRelative("_playerAttributes");
+
         // drawing instruction
         Rect foldOutBox = new Rect(position.min.x, position.min.y, position.size.x, EditorGUIUtility.singleLineHeight);
 
         property.isExpanded = EditorGUI.Foldout(foldOutBox, property.isExpanded, label);
 
 
-        // Start to draw properties
+        if (property.isExpanded)
+        {
+            // Start to draw properties
+            DrawAbilityProperty(position);
+
+
+
+
+        }
 
         EditorGUI.EndProperty();
     }
@@ -32,20 +43,29 @@ public class PlayerAbilityDrawer : PropertyDrawer
     // request more vertical spacing, return it
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        int totalLines = 2;
+        int totalLines = 1;
 
 
         // increase the height if expand arrow
         if (property.isExpanded)
         {
-            totalLines += 3;
+            totalLines += 2;
         }
-
-
 
         return (EditorGUIUtility.singleLineHeight * totalLines);
 
+    }
 
+
+    void DrawAbilityProperty(Rect position)
+    {
+        float xPos = position.min.x;
+        float yPos = position.min.y + EditorGUIUtility.singleLineHeight;
+        float width = position.size.x;
+        float height = EditorGUIUtility.singleLineHeight;
+
+        Rect drawRect = new Rect(xPos, yPos, width, height);
+        EditorGUI.PropertyField(drawRect, _playerAttributes, new GUIContent("PlayerAttributes"));
     }
 
 
